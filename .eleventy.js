42558c4f2cbe8fbc14bf7ec516417bc6866ffadf
Hadley Rose <hadleyrosemitchell@gmail.com>
2025-07-03 16:19:06 -0700

    Update 11ty config with shortcodes and pass thru js dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
 // eleventy config
+const shortcodes = require('./src/js/shortcodes');
+
 module.exports = function(eleventyConfig) {
+    // include js
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addWatchTarget("./src/js");
+
+    // custom shortcodes
+    eleventyConfig.addPairedShortcode('centerWindow', shortcodes.centerWindowShortcode);
+
     // define input and output dirs
     return {
         dir: {
