a743da3f84fbf3b0a795c022af49d0f865e38e48
Hadley Rose <hadleyrosemitchell@gmail.com>
2025-07-03 17:05:41 -0700

    Fix markdown parsing with explicit markdown rendering in shortcodes

--- src/js/shortcodes/window.js
+++ src/js/shortcodes/window.js
@@ -1,13 +1,29 @@
+import markdownIt from 'markdown-it';
+
+// set markdown options to parse html, \n, and links
+const options = {
+    html: true,
+    breaks: true,
+    linkify: true
+};
+
+// create markdownit instance and disable parsing indented text as code
+const md = markdownIt(options).disable('code');
+
+// create a centered window element
+// and parse contents as markdown
 export function centerWindow(children) {
     if (!children) {
         throw new Error('Children must be non-empty string.');
     }
-    return `<div class="window" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%)">${children}</div>`;
+    return `<div class="window" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%)">${md.render(children)}</div>`;
 };
 
+// create a window element
+// and parse contents as markdown
 export function window(children) {
     if (!children) {
         throw new Error('Children must be non-empty string.');
     }
-    return `<div class="window">${children}</div>`;
+    return `<div class="window">${md.render(children)}</div>`;
 };
