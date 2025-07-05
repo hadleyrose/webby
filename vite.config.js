90260991b2a451245d912acc96d03c64f5322585
Hadley Rose <hadleyrosemitchell@gmail.com>
2025-07-05 01:01:13 -0700

    Update vite config with new root, out, and public dirs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-    root: 'public',
+    root: '_site',
     build: {
-        outDir: '../dist',
+        outDir: '../public',
         emptyOutDir: true
     },
-    publicDir: 'assets',
+    // publicDir: 'src',
     assetsInclude: ["**/*.glb"]
 })
