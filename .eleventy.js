cec9eb3562cbcdd66a89d033831d4f282ff3736d
Hadley Rose <hadleyrosemitchell@gmail.com>
2025-07-04 21:34:15 -0700

    Update 11ty config to pass thru and watch assets dir

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,13 @@
 import { windowShortcode } from './src/js/shortcodes/index.js';
 
 export default (eleventyConfig) => {
-    // include js, css
+    // include js, css, assets
     eleventyConfig.addPassthroughCopy("./src/js");
     eleventyConfig.addWatchTarget("./src/js");
     eleventyConfig.addPassthroughCopy("./src/styles");
     eleventyConfig.addWatchTarget("./src/styles");
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addWatchTarget("./src/assets");
 
     // custom shortcodes
     eleventyConfig.addPairedShortcode('window', windowShortcode);
