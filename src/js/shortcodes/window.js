2d82c526e10fac9775ce74aeefdf5bbe802b4a85
Hadley Rose <hadleyrosemitchell@gmail.com>
2025-07-18 09:25:50 -0700

    Add id to window Close button

--- src/js/shortcodes/window.js
+++ src/js/shortcodes/window.js
@@ -24,7 +24,7 @@ export function window(content, titleBarTitle = 'test', center = true, type = "i
             <div class="title-bar-controls">
                 <button aria-label="Minimize"></button>
                 <button aria-label="Maximize"></button>
-                <button aria-label="Close"></button>
+                <button id="Close" aria-label="Close"></button>
             </div>
         </div>
         <div class="window-body">
